4b0acdc5b67f3bb65f0191f990b45d643adbd8af
Ron Wellman <ron.wellman01@gmail.com>
2025-03-22 12:19:29 -0400

    Removed unused variables and imports.

--- src/main/java/com/courseproject/inventoryservice/services/PurchaseOrderService.java
+++ src/main/java/com/courseproject/inventoryservice/services/PurchaseOrderService.java
@@ -6,7 +6,6 @@ import com.courseproject.inventoryservice.models.PurchaseOrderLineItem;
 import com.courseproject.inventoryservice.models.Vendor;
 import com.courseproject.inventoryservice.models.dto.PointOfSaleProductDTO;
 import com.courseproject.inventoryservice.models.enums.PurchaseOrderStatus;
-import com.courseproject.inventoryservice.repositories.PurchaseOrderLineItemRepository;
 import com.courseproject.inventoryservice.repositories.PurchaseOrderRepository;
 import com.courseproject.inventoryservice.services.utils.PointOfSaleFeignClient;
 import jakarta.persistence.EntityNotFoundException;
@@ -17,15 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class PurchaseOrderService {
     private final PurchaseOrderRepository purchaseOrderRepository;
-    private final PurchaseOrderLineItemRepository purchaseOrderLineItemRepository;
     private final ProductService productService;
     private final VendorService vendorService;
     private final PointOfSaleFeignClient pointOfSaleFeignClient;
@@ -44,10 +40,13 @@ public class PurchaseOrderService {
         return purchaseOrderRepository.save(purchaseOrder);
     }
 
-    public PurchaseOrder updatePurchaseOrder(Long id, PurchaseOrder purchaseOrder) throws EntityNotFoundException, OptimisticLockingFailureException {
+    public PurchaseOrder updatePurchaseOrder(Long id, PurchaseOrder purchaseOrder)
+            throws EntityNotFoundException, OptimisticLockingFailureException {
         PurchaseOrder oldPurchaseOrder = findPurchaseOrderById(id);
-        if (purchaseOrder.getOrderDate() != null) oldPurchaseOrder.setOrderDate(purchaseOrder.getOrderDate());
-        if (purchaseOrder.getDeliveryDate() != null) oldPurchaseOrder.setDeliveryDate(purchaseOrder.getDeliveryDate());
+        if (purchaseOrder.getOrderDate() != null)
+            oldPurchaseOrder.setOrderDate(purchaseOrder.getOrderDate());
+        if (purchaseOrder.getDeliveryDate() != null)
+            oldPurchaseOrder.setDeliveryDate(purchaseOrder.getDeliveryDate());
         return purchaseOrderRepository.save(oldPurchaseOrder);
     }
 
@@ -58,8 +57,9 @@ public class PurchaseOrderService {
     public PurchaseOrder receivePurchaseOrder(String token, Long id)
             throws EntityNotFoundException, OptimisticLockingFailureException, IllegalArgumentException {
         PurchaseOrder purchaseOrder = findPurchaseOrderById(id);
-        if (purchaseOrder.getPurchaseOrderLineItems().isEmpty()) return null;
-        Set<Product> updatedProducts = new HashSet<>();
+        if (purchaseOrder.getPurchaseOrderLineItems().isEmpty())
+            return null;
+
         for (PurchaseOrderLineItem lineItem : purchaseOrder.getPurchaseOrderLineItems()) {
             Product product = lineItem.getProduct();
             if (product == null)
@@ -71,12 +71,12 @@ public class PurchaseOrderService {
 
             Product savedProduct = productService.saveProduct(product);
             try {
-            ResponseEntity<Product> response = pointOfSaleFeignClient.createProduct(token, new PointOfSaleProductDTO(
-                    savedProduct.getId(),
-                    savedProduct.getName(),
-                    savedProduct.getDescription(),
-                    savedProduct.getPrice()
-            ));
+                ResponseEntity<Product> response = pointOfSaleFeignClient.createProduct(token,
+                        new PointOfSaleProductDTO(
+                                savedProduct.getId(),
+                                savedProduct.getName(),
+                                savedProduct.getDescription(),
+                                savedProduct.getPrice()));
                 if (!response.getStatusCode().is2xxSuccessful()) {
                     throw new RuntimeException("Failed to save product " + savedProduct.getId());
                 }
@@ -91,7 +91,8 @@ public class PurchaseOrderService {
     @Transactional
     public PurchaseOrder addLineItemToPurchaseOrder(Long id, List<PurchaseOrderLineItem> lineItems)
             throws EntityNotFoundException, OptimisticLockingFailureException, IllegalArgumentException {
-        if (lineItems == null || lineItems.isEmpty()) throw new IllegalArgumentException("Line items is null.");
+        if (lineItems == null || lineItems.isEmpty())
+            throw new IllegalArgumentException("Line items is null.");
         PurchaseOrder purchaseOrder = findPurchaseOrderById(id);
         lineItems.forEach(lineItem -> {
             Product product = productService.getProductById(lineItem.getProduct().getId());
